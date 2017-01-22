408747a860e68c6d8d2a2f83fadb4189df19d5f7
Elliot Cuzzillo <elliotc@unity3d.com>
2017-01-21 17:09:49 -0800

    downforce

--- Project Folder/Assets/Scripts/BoatController.cs
+++ Project Folder/Assets/Scripts/BoatController.cs
@@ -3,56 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BoatController : MonoBehaviour {
-
-    Rigidbody rb;
-    public float rotSpeed = .5f;
-    public float speed = 5f;
-    public float breakSpeed = 10f;
-    public string inputHrz = "Horizontal_p1";
-    public string inputVrt = "Vertical_p1";
-    public string inputFire = "Fire1_p1";
+	public Terrain terrain;
+	public TerrainData tdat;
+    public Rigidbody rb;
+    public float rotSpeed;
+    public float speed;
+    public float breakSpeed;
+	public float downforce;
+    public string inputHrz;
+    public string inputVrt;
+    public string inputFire;
     public GameObject bulletPrefab;
     public GameObject cam;
 
-    // Use this for initialization
-    void Start () {
-        rb = gameObject.GetComponent<Rigidbody>();
+	void Start() {
+		tdat = terrain.terrainData;
+	}
+
+	void FixedUpdate() {
+        rb.AddForce(cam.transform.forward * -Input.GetAxis(inputVrt) * speed);
+        rb.AddForce(cam.transform.right * Input.GetAxis(inputHrz) * speed);
+
+		var localpos = terrain.transform.InverseTransformPoint(transform.position);
+		var normal = tdat.GetInterpolatedNormal(localpos.x / tdat.size.x,
+												localpos.z / tdat.size.z);
+
+		rb.AddForce(-normal * downforce);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        //if (Input.GetKey(inputRight))
-        //{
-        //    transform.Rotate(0, rotSpeed, 0);
-        //}
-        //if (Input.GetKey(inputLeft))
-        //{
-        //    transform.Rotate(0, -rotSpeed, 0);
-
-        //}
-        //if (Input.GetKey(inputThrottle))
-        //{
-        //    rb.AddForce(transform.forward * speed);
-        //}
-        //if (Input.GetKey(inputBreak))
-        //{
-        //    if (rb.velocity.x > 0)
-        //    {
-        //        rb.AddForce(-transform.forward * speed);
-        //    }
-        //}
-        //if (Input.GetKeyDown(inputFire))
-        //{
-        //    Vector3 bulletSource = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        //    GameObject bullet = Instantiate(bulletPrefab, bulletSource, transform.rotation);
-        //    //set bullet speed?
-        //    //set shooter to this to prevent friendly fire
-
-        //}
-
-        rb.AddForce(cam.transform.forward * -Input.GetAxis(inputVrt) * speed);
-        rb.AddForce(cam.transform.right * Input.GetAxis(inputHrz) * speed);
 
         if (Input.GetButtonDown(inputFire))
         {
